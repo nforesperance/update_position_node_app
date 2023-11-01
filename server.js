5f9e8af766c9b1316caec331e1c057620598a51e
Nfor Esperance <nforesperance1@gmail.com>
2023-11-01 16:19:51 +0100

    updates

--- server.js
+++ server.js
@@ -1,28 +1,42 @@
 'use strict';
 var amqp = require('amqplib/callback_api');
 
-function sendDataByAmqp(queue, data) {
-    amqp.connect('amqp://localhost', function (error0, connection) {
+
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const QUEUE_NAME = 'update_position';
+// amqp_url with username and password
+const amqp_url = 'amqp://test:test@localhost:5672';
+
+// App
+const app = express();
+app.use(express.json());
+
+function sendDataByAmqp(data) {
+    amqp.connect(amqp_url, function (error0, connection) {
         if (error0) {
-             console.log(error0);
+            console.log(error0);
             return -1;
 
         }
         connection = connection;
         connection.createChannel(function (error1, channel) {
             if (error1) {
-                 console.log(error1);
+                console.log(error1);
                 return -1;
             }
-            channel.assertQueue(queue, {
+            channel.assertQueue(QUEUE_NAME, {
                 durable: false
             });
-            channel.sendToQueue(queue, Buffer.from(data));
+            channel.sendToQueue(QUEUE_NAME, Buffer.from(data));
             console.log(" [x] Sent %s", data);
 
             setTimeout(function () {
                 connection.close();
-                process.exit(0)
             }, 500);
             return 0;
         });
@@ -30,18 +44,6 @@ function sendDataByAmqp(queue, data) {
 
 
 }
-
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.use(express.json());
-
-
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
@@ -73,9 +75,7 @@ app.post('/update_position', (req, res) => {
         "longitude": 12.123456,
         "client": "nodejs",
     }
-    var sent = sendDataByAmqp('update_position', JSON.stringify(data));
-    console.log(sent);
-
+    var sent = sendDataByAmqp(JSON.stringify(data));
     res.json(data);
 });
 
