085720238057082a3847929390eeb31b26989334
Nfor Esperance <nforesperance1@gmail.com>
2023-11-04 11:45:52 +0100

    reusable connection and channels

--- server.js
+++ server.js
@@ -6,16 +6,138 @@ var amqp = require('amqplib/callback_api');
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = 8081;
 const HOST = '0.0.0.0';
 const QUEUE_NAME = 'update_position';
 // amqp_url with username and password
+// Define the connection URL and the queue name
 const amqp_url = 'amqp://test:test@localhost:5672';
+// Declare global variables for the connection and the channel
+var connection = null;
+var channel = null;
+
+// Define a function that creates the connection and the channel
+function createConnectionAndChannel(callback) {
+    // Connect to the broker using the URL
+    amqp.connect(amqp_url, function (err, conn) {
+        // If there is an error, log it and retry after some time
+        if (err) {
+            console.error('[AMQP]', err.message);
+            return setTimeout(createConnectionAndChannel, 1000, callback);
+        }
+        // Set the connection variable and register the event handlers
+        connection = conn;
+        connection.on('error', function (err) {
+            if (err.message !== 'Connection closing') {
+                console.error('[AMQP] conn error', err.message);
+            }
+        });
+        connection.on('close', function () {
+            console.error('[AMQP] reconnecting');
+            return setTimeout(createConnectionAndChannel, 1000, callback);
+        });
+        connection.on('blocked', function (reason) {
+            console.warn('[AMQP] blocked', reason);
+        });
+        connection.on('unblocked', function () {
+            console.info('[AMQP] unblocked');
+        });
+        // Create the channel using the connection
+        createChannel(connection, callback);
+
+
+    });
+}
+
+function createChannel(connection, callback) {
+    // Create the channel using the connection
+    connection.createChannel(function (err, ch) {
+        // If there is an error, log it and close the connection
+        if (err) {
+            console.error('[AMQP] channel error', err.message);
+            connection.close();
+            return setTimeout(createConnectionAndChannel, 1000, callback);
+            return;
+        }
+        // Set the channel variable and register the event handlers
+        channel = ch;
+        channel.on('error', function (err) {
+            console.error('[AMQP] channel error', err.message);
+            connection.close();
+            return setTimeout(createConnectionAndChannel, 1000, callback);
+        });
+        channel.on('close', function () {
+            console.log('[AMQP] channel closed');
+        });
+        // Assert the queue using the channel
+        channel.assertQueue(QUEUE_NAME, { durable: false });
+        // Call the callback function if provided
+        if (callback) {
+            callback();
+        }
+    });
+}
+
+
+
+
+
+// Define a function that sends a message to the queue using the channel
+function sendMessage(message) {
+    // If the channel is not ready, wait until it is
+    if (!channel) {
+        return createConnectionAndChannel(function () {
+            sendMessage(message);
+        });
+    }
+    // Convert the message to a buffer and send it to the queue
+    var buffer = Buffer.from(message);
+    channel.sendToQueue(QUEUE_NAME, buffer, { persistent: true, contentType: 'update_position', });
+    console.log('[AMQP] sent', message);
+}
+
+// Define a function that receives a message from the queue using the channel
+function receiveMessage() {
+    // If the channel is not ready, wait until it is
+    if (!channel) {
+        return createConnectionAndChannel(function () {
+            receiveMessage();
+        });
+    }
+    // Consume the queue and handle the messages
+    channel.consume(QUEUE_NAME, function (msg) {
+        if (msg !== null) {
+            var message = msg.content.toString();
+            console.log('[AMQP] received', message);
+            // Do something with the message here
+            // ...
+            // Acknowledge the message
+            channel.ack(msg);
+        }
+    });
+}
+
+// Test the functions
+sendMessage('Hello World!');
+// receiveMessage();
+
+
+
 
 // App
 const app = express();
 app.use(express.json());
 
+
+
+// amqp.connect().then(function (conn) {
+//     // Everything ok, but ..
+//     throw new Error('SNAFU');
+// }, function (err) {
+//     console.error('Connect failed: %s', err);
+// }).then(null, function (err) {
+//     console.error('Connect succeeded, but error thrown: %s', err);
+// });
 function sendDataByAmqp(data) {
     amqp.connect(amqp_url, function (error0, connection) {
         if (error0) {
@@ -44,6 +166,12 @@ function sendDataByAmqp(data) {
 
 
 }
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
@@ -75,7 +203,7 @@ app.post('/update_position', (req, res) => {
         "longitude": 12.123456,
         "client": "nodejs",
     }
-    var sent = sendDataByAmqp(JSON.stringify(data));
+    var sent = sendMessage(JSON.stringify(data));
     res.json(data);
 });
 
@@ -83,6 +211,15 @@ app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
 const gracefulShutdown = () => {
+    console.log('Received kill signal, shutting down gracefully');
+    if (connection) {
+        connection.close(() => {
+            console.log('Closed out remaining connections');
+            process.exit(0);
+        });
+    } else {
+        process.exit(0);
+    }
 };
 
 process.on('SIGINT', gracefulShutdown);
